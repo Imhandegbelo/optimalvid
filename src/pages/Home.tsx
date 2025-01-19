4e68ebfb67a79b3177b86c602a06f480d4ab8859
Imhandegbelo <abramgeo5@gmail.com>
2025-01-19 20:18:35 +0100

    style: added hero to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import right_arrow from "../assets/ArrowRight.svg"
+import right_arrow from "../assets/ArrowRight.svg";
 import movies from "../data/movies.json";
 import MovieGrid from "../component/MovieGrid";
+import hero from "../assets/thumbnail.jpg";
 
 const Home: React.FC = () => {
   return (
     <div className="px- md:px-12 lg:px-16">
+      <div
+        className="relative h-[400px] mt-6"
+        style={{ backgroundImage: `url(${hero})` }}
+      >
+        <div className="absolute flex items-center justify-center text-white text-2xl md:text-5xl bg-center bg-cover inset-0 bg-black/70">
+          OptimalVid
+        </div>
+      </div>
       <div className="py-10">
         <div className="flex justify-between w-full items-center mb-11">
           <h2 className="text-black text-2xl sm:text-3xl font-semibold">
